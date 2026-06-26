44634079cf0971a3724cc0481b41ad67f437fe62
zhouqinglong520 <44882914+zhouqinglong520@users.noreply.github.com>
2026-06-26 08:57:23 +0800

    Upgrade SW to v4 - fix mobile detail view

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 游资心法 PWA Service Worker - 离线优先 + 网络回退
-const CACHE = 'youzi-reader-v2';
+const CACHE = 'youzi-reader-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (e) => {
